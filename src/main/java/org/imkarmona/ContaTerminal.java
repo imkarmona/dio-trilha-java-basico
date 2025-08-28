9ac44d5a970574899a56eb40bd3e06e4a9c6be89
imkarmona <guilherme.carmona86@gmail.com>
2025-08-28 19:09:35 -0300

    desafio-1-dio

--- src/main/java/org/imkarmona/ContaTerminal.java
+++ src/main/java/org/imkarmona/ContaTerminal.java
@@ -1,7 +1,27 @@
 package org.imkarmona;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int number;
+        String agency;
+        String customerName;
+        double balance;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Por favor, digite o número da agência: ");
+        agency = scanner.nextLine();
+        System.out.println("Por favor, digite o nome do cliente: ");
+        customerName = scanner.nextLine();
+        System.out.println("Por favor, digite o saldo da conta: ");
+        balance = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Olá " + customerName + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + number + " e seu saldo " + balance + " já está disponível para saque.");
     }
 }
